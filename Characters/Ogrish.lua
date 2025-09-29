7b3a151534e48f690c3b2ef2e7779cad5e3a670a
odeebee <bargerybill@gmail.com>
2025-09-29 17:16:08 +0000

    Add files via upload

--- Characters/Ogrish.lua
+++ Characters/Ogrish.lua
@@ -2,6 +2,8 @@ require "singleton"
 require "player"
 
 Ogrish = {
+    displaySpeechIndicator = false,
+    speechBounds = 50,
     canDraw = false,
     location = "Docks",
     x = 400,
@@ -14,14 +16,27 @@ Ogrish = {
                 "You should start thinking about finding a place to stay.",
                 "Theres a motel just down the road",
                 "Travel safe detective."},
+    dialogeIndex = 1,
     
     boundaryChecker = function()
-        print("Boundary")
+        --print(player.xPosition)
+        if player.xPosition >= Ogrish.x - Ogrish.speechBounds and player.xPosition <= Ogrish.x + Ogrish.width + Ogrish.speechBounds then
+            Ogrish.displaySpeechIndicator = true
+        else
+            Ogrish.displaySpeechIndicator = false
+        end
     end,
 
     drawCharacter = function()
         if Ogrish.canDraw == true then
-            love.graphics.rectangle("fill",Ogrish.x,floorlevel-Ogrish.height,Ogrish.width,Ogrish.height)
+            love.graphics.rectangle("fill",
+                                    Ogrish.x,
+                                    floorlevel-Ogrish.height,
+                                    Ogrish.width,
+                                    Ogrish.height)
+            if Ogrish.displaySpeechIndicator == true then
+                love.graphics.print("Press X", Ogrish.x,floorlevel-Ogrish.height-20)
+            end
         end
     end
 }
--- main.lua
+++ main.lua
@@ -13,6 +13,8 @@ table.insert(chars, Ogrish)
 local tween = require "tween"
 local t1
 
+dialogeDisplayText = ""
+
 function love.load()
     love.window.setMode(screenWidth,screenHeight)
     levelIndex = 1 --Docks
@@ -31,11 +33,24 @@ function love.draw()
     drawPlayer()
     drawLevels()
     drawCharacters()
+    drawDialogeText()
 end
 
 function love.mousepressed(x,y,button,istouch,presses)
     if button == 1 then
         movePlayer(x)
+    end
+end
+
+function love.keypressed(key)
+    if key == "x" then
+        for i=1,#chars do
+            if chars[i].displaySpeechIndicator == true then
+                if chars[i].dialogeIndex == 1 then
+                    dialogeDisplayText = chars[i].dialoge[chars[i].dialogeIndex]
+                end
+            end
+        end
     end
 end
 
@@ -65,6 +80,10 @@ function movePlayer(x)
     if time > 0 then
         t1 = tween.new(time,player,{xPosition = x})
     end
+end
+
+function drawDialogeText()
+    love.graphics.print(dialogeDisplayText)
 end
 
 function updateCharsBoundaries()
